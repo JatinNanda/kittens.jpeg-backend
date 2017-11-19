b64cc8711692f3f130ce17bf1e33e2b8b29bba9a
EC2 Default User <ec2-user@ip-172-31-80-216.ec2.internal>
2017-11-19 17:28:25 +0000

    script changes before refactor

--- scripts/data/processing_scripts/minimize_json.py
+++ scripts/data/processing_scripts/minimize_json.py
@@ -2,14 +2,14 @@ import json
 from os import listdir
 from os.path import isfile, join
 
-mypath = "./"
-onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f)) and ".py" not in f]
+mypath = "../nyt-files/archives-json/"
+onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f)) and ".py" not in f and ".zip" not in f]
 
 for f in onlyfiles:
     print f
     data = None
-    with open(f, "rb") as infile:
+    with open(mypath + f, "rb") as infile:
         data = json.load(infile)
-    with open(f, "wb") as outfile:
+    with open(mypath + f, "wb") as outfile:
         json.dump(data, outfile)
 
